fe520ee6558765f163cf4be5fd1d5d46bf58e68f
Norbi <norbimacsali445@gmail.com>
2021-05-15 16:32:18 +0200

    Init

---
+++ app/src/main/java/com/example/fitnessaplikacia/fragments/RunFragment.kt
@@ -0,0 +1,14 @@
+package com.example.fitnessaplikacia.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fitnessaplikacia.R
+
+class RunFragment : Fragment(R.layout.fragment_run) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/fitnessaplikacia/fragments/WeightFragment.kt
@@ -0,0 +1,14 @@
+package com.example.fitnessaplikacia.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fitnessaplikacia.R
+
+class WeightFragment : Fragment(R.layout.fragment_weight) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
