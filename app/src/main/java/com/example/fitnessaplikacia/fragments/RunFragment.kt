d9ce7b49c74032d3d54069b968c11fa0915e8fa7
Norbi <norbimacsali445@gmail.com>
2021-05-18 17:22:17 +0200

    Designing StartRunFragment and SettingsFragment

--- app/src/main/java/com/example/fitnessaplikacia/fragments/RunFragment.kt
+++ app/src/main/java/com/example/fitnessaplikacia/fragments/RunFragment.kt
@@ -2,20 +2,14 @@ package com.example.fitnessaplikacia.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.example.fitnessaplikacia.R
-import kotlinx.android.synthetic.main.fragment_weight.*
 
-class WeightFragment : Fragment(R.layout.fragment_weight) {
+
+class RunFragment : Fragment(R.layout.fragment_run) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        npFirst.maxValue = 150
-        npFirst.minValue = 0
-        npFirst.value = 80
-
-        npSecond.maxValue = 9
-        npSecond.minValue = 0
-        npSecond.value = 0
     }
 }
